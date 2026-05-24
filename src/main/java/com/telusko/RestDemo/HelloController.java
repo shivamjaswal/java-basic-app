0243cfe8f58b46db6643778f82e4dece2cf29e52
shivamjaswal <theshivamjaswal@gmail.com>
2026-05-24 14:10:46 +0530

    first commit

---
+++ src/main/java/com/telusko/RestDemo/HelloController.java
@@ -0,0 +1,15 @@
+package com.telusko.RestDemo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String getData() {
+        return "Hello World from Instance";
+    }
+
+}
